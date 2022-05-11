e42459a86183727977b284ebf3db4a5778416870
Kedzie <kedziestark@gmail.com>
2022-05-11 15:29:19 -0400

    Create pseudocode for playRound function

--- game.js
+++ game.js
@@ -1,12 +1,4 @@
 //create function that returns rock paper or scissors...
-
-
-//create a new function called computerPlay
-//generate a random number 1-3
-//if 1, return Rock
-//else if 2, return Paper
-//else 3, return Scissors
-
 function computerPlay() {
     let play = Math.floor(Math.random() * 3);
     if (play == 0) {
@@ -19,3 +11,20 @@ function computerPlay() {
 }
 
 computerPlay();
+
+//write a function that plays a single round...
+
+//create playRound function
+//accept two parameters-- playerSelection and computerSelection
+//ask player for playerSelection
+    //convert players input to lowercase
+    //if input unacceptable, ask again
+
+//if input is acceptable...
+//compare the two parameters
+//if  playerSelection == computerSelection, call a tie
+    //print 'you both called (selection)
+//if (pS = rock AND cS = paper) OR (pS = paper AND cS = scissors) OR (pS = scissors AND cS = rock)
+    //print 'you lose! %{cS} beats %{pS}'
+//else
+    //print 'you win! %{pS} beats %{cS}
